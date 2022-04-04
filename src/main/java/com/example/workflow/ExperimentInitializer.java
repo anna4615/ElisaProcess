a3557bf37242a42ac58389380bc4a0dafa582ded
anna <anna4615@hotmail.com>
2022-04-04 09:51:18 +0200

    Refactored ExperimentInitializer

--- src/main/java/com/example/workflow/ExperimentInitializer.java
+++ src/main/java/com/example/workflow/ExperimentInitializer.java
@@ -1,32 +1,20 @@
 package com.example.workflow;
-
-import camundajar.impl.scala.collection.Map;
-import com.example.workflow.models.Elisa;
 import com.example.workflow.models.*;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.json.UTF8DataInputJsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.camunda.spin.Spin;
 import org.camunda.spin.SpinList;
 import org.camunda.spin.json.SpinJsonNode;
 import org.springframework.stereotype.Component;
-import spinjar.com.minidev.json.JSONValue;
 import org.json.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.camunda.spin.Spin.*;
-import static org.camunda.spin.DataFormats.*;
 
 
 @Component("ExperimentInitializer")
@@ -34,35 +22,18 @@ public class ExperimentInitializer implements  JavaDelegate{
 
     private static final String LIMS_API_URL = "http://localhost:3627/graphql";
     private HttpClient client = HttpClient.newHttpClient();
+    private int elisaId;
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
 
-        int elisaId = createElisa();
+        elisaId = createElisa();
         execution.setVariable("ElisaId", elisaId);
 
-        String sampleString = (String) execution.getVariable("samples");
-        String[] samplesString = sampleString.split(";");
-        //TODO: felhantering om fler än 72 prover, dvs samplesString.length > 72
-        Sample[] samples = new Sample[samplesString.length];
+        String[] samplesInput = ((String) execution.getVariable("samples")).split(";");
+        //TODO: felhantering om fler än 72 prover, dvs samplesInput.length > 72
 
-        //TODO: använd Spin för att mappa Json till Sample
-        for (int i = 0; i < samples.length; i++) {
-            JSONObject sampleJson = new JSONObject(samplesString[i]);
-            int id = sampleJson.getInt("id");
-            String name = sampleJson.getString("name");
-            Sample sample = new Sample(id, name);
-            samples[i] = sample;
-        }
-
-        ArrayList<Test> testList = new ArrayList<>();
-        int position = 1;
-
-        for (int i = 0; i < samples.length; i++) {
-            Test test = createTest(samples[i].getId(), samples[i].getName(), elisaId, position);
-            testList.add(test);
-            position++;
-        }
+        ArrayList<Test> testList = createTestList(samplesInput);
 
         String tests = JSON(testList).toString();
         execution.setVariable("tests", tests);
@@ -91,7 +62,22 @@ public class ExperimentInitializer implements  JavaDelegate{
         return id;
     }
 
-    private Test createTest(int sampleId, String sampleName, int elisaId, int position) throws IOException, InterruptedException {
+    private ArrayList<Test> createTestList(String[] samplesInput) throws IOException, InterruptedException {
+
+        ArrayList<Test> testList = new ArrayList<>();
+        int position = 1;
+
+        for (String input : samplesInput){
+            JSONObject inputJson = new JSONObject(input);
+            Test test = createTest(inputJson.getInt("id"), inputJson.getString("name"), position);
+            testList.add(test);
+            position++;
+        }
+
+        return testList;
+    }
+
+    private Test createTest(int sampleId, String sampleName, int position) throws IOException, InterruptedException {
 
         String query = "{\"query\":\"mutation{addTest(input:{sampleId:" + sampleId +
                                                             ",elisaId:" + elisaId +
