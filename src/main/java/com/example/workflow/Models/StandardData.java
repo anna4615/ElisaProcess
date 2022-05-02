72f6ce2182c83f7a9f6dd6d34e029dd70489ba8e
annsj <anna4615@hotmail.com>
2022-05-02 19:45:12 +0200

    changed property name in StandardData

--- src/main/java/com/example/workflow/Models/StandardData.java
+++ src/main/java/com/example/workflow/Models/StandardData.java
@@ -2,21 +2,21 @@ package com.example.workflow.Models;
 
 public class StandardData {
 
-    private int position;
+    private int pos;
     private float concentration;
     private float measValue;
 
     public StandardData() {
     }
 
-    public StandardData(int position, float concentration, float value) {
-        this.position = position;
+    public StandardData(int pos, float concentration, float value) {
+        this.pos = pos;
         this.concentration = concentration;
         this.measValue = value;
     }
 
-    public int getPosition() {
-        return position;
+    public int getPos() {
+        return pos;
     }
 
     public float getConcentration() {
